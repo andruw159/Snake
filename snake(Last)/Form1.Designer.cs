6c9b8a1e9ca4d54d085cca44c3d73ab91d35bf93
andruw159 <andres.bernalm@udea.edu.co>
2023-11-17 23:25:12 -0500

    First Commit Snake

---
+++ snake(Last)/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace snake_Last_
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntos.Font = new System.Drawing.Font("Eras Medium ITC", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.Location = new System.Drawing.Point(525, 360);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(64, 69);
+            this.lblPuntos.TabIndex = 0;
+            this.lblPuntos.Text = "0";
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::snake_Last_.Properties.Resources.backg2;
+            this.ClientSize = new System.Drawing.Size(601, 438);
+            this.Controls.Add(this.lblPuntos);
+            this.Name = "Snake";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MoverPieza);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPuntos;
+    }
+}
+
---
+++ snake(Last)/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_Last_
+{
+    public partial class Snake : Form
+    {
+        List<PictureBox> lista = new List<PictureBox>();
+        int sizePiezaPrincipal = 15;
+        int tiempo = 10;
+        PictureBox comida = new PictureBox();
+        String direccion = "Right"; //Dirección de la serpiente
+
+        public Snake()
+        {
+            InitializeComponent();
+            iniciarJuego();
+        }
+
+        public void ReiniciarJuego()
+        {
+            foreach (PictureBox Serpiente in lista) { this.Controls.Remove(Serpiente); }
+            this.Controls.Remove(comida); //No va remove comida sino que hay que hacer remove la serepiente?
+            iniciarJuego();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int nx = lista[0].Location.X;
+            int ny = lista[0].Location.Y;
+            lista[0].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Head" + direccion);//Cabeza de la serpiente
+
+
+            for (int i = lista.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    if (direccion == "Right") nx = nx + sizePiezaPrincipal;
+                    else if (direccion == "Left") nx = nx - sizePiezaPrincipal;
+                    else if (direccion == "Up") ny = ny - sizePiezaPrincipal;
+                    else if (direccion == "Down") ny = ny - sizePiezaPrincipal;
+                    lista[0].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Head" + direccion);//Cabeza de la serpiente
+                    lista[0].Location = new Point(nx, ny);
+                }
+
+                else
+                {
+                    //Intercambio de seguimiento
+                    lista[i].Location = new Point((lista[i - 1].Location.X), (lista[i].Location.Y));
+                    lista[i].Location = new Point((lista[i].Location.X), (lista[i - 1].Location.Y));
+                }
+            }
+            for (int contarPiezas = 1; contarPiezas <= lista.Count; contarPiezas++)
+            {
+                if (lista[contarPiezas].Bounds.IntersectsWith(comida.Bounds))
+                {
+                    this.Controls.Remove(comida); //remueve la comida
+                    tiempo = Convert.ToInt32(timer1.Interval);//aumenta el tiempo
+                    if (tiempo > 10) { timer1.Interval = tiempo - 10; }
+                    lblPuntos.Text = (Convert.ToInt32(lblPuntos.Text) + 1).ToString();
+                    crearComida();//crea nueva comida
+                    crearSnake(lista, this, lista[lista.Count - 1].Location.X * sizePiezaPrincipal, 0);// Nueva parte de la snake
+
+                }
+            }
+            //detectar colision con paredes
+            if ((lista[0].Location.X >= this.Width - 15) || (lista[0].Location.Y >= this.Height - 50) || (lista[0].Location.Y < -10) || (lista[0].Location.X < -30))
+            {
+                ReiniciarJuego();
+            }
+
+            //colision con el cuerpo
+            for (int contarPiezas = 1; contarPiezas < lista.Count; contarPiezas++)
+            {
+                if (lista[0].Bounds.IntersectsWith(lista[contarPiezas].Bounds))
+                {
+                    ReiniciarJuego();
+                }
+            }
+        }
+
+        private void MoverPieza(object sender, KeyEventArgs e)
+        {
+            direccion = ((e.KeyCode & Keys.Up) == Keys.Up) ? "Up" : direccion;
+            direccion = ((e.KeyCode & Keys.Down) == Keys.Down) ? "Down" : direccion;
+            direccion = ((e.KeyCode & Keys.Left) == Keys.Left) ? "Left" : direccion;
+            direccion = ((e.KeyCode & Keys.Right) == Keys.Right) ? "Right" : direccion;
+
+        }
+
+        public void crearSnake(List<PictureBox> listaPelota, Form formulario, int posicionx, int posiciony)
+        {
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(posicionx, posiciony);
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("body");
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            listaPelota.Add(pb);
+            formulario.Controls.Add(pb);
+        }
+
+        private void crearComida()
+        {
+            Random rnd = new Random();
+            int entx = rnd.Next(1, this.Width - sizePiezaPrincipal - 10);
+            int enty = rnd.Next(1, this.Height - sizePiezaPrincipal - 40);
+
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(entx, enty);
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("manzanaa");
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            comida = pb;
+            this.Controls.Add(pb);
+
+        }
+
+        public void iniciarJuego()
+        {
+            tiempo = 10;
+            direccion = "Right";
+            timer1.Interval = 200;
+            lista = new List<PictureBox>();
+
+            //Piezas iniciales
+            for (int i = 2; 0 <= i; i--)
+            {
+                crearSnake(lista, this, (i * sizePiezaPrincipal) + 100, 80);
+            }
+            crearComida();
+        }
+    }
+}
